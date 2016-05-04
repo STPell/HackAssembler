466691797ef9cb3499502107fd93913b2c8a2e68
STPell <pellsamuel@gmail.com>
2016-05-04 19:32:12 +1200

    AssemblyCleaner
    Cleans up the inputted HACK assembly language so that the HackTranslator
    module can translate it into the CPU instructions.

---
+++ HackTranslator.py
@@ -0,0 +1,16 @@
+"""
+   Hack Assembly Language Translator
+   Provides tools and methods to translate cleaned HACK assembly code into
+   HACK CPU instructions
+   -----------------------------------------------------------------------
+   Contains: translate
+             find_references
+"""
+   
+def translate(assembly_instructions):
+    """
+       Translates HACK assembly language into HACK CPU code
+       Takes a list of assembly instructions, and returns a list of 
+       CPU instructions.
+    """
+    pass
